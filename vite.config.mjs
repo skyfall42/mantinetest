5199d19b8b83a47f9816169a4843df02deedaaf8
L.A. Morales <33805549+skyfall42@users.noreply.github.com>
2025-02-05 09:49:12 +0800

    Initialize sass-embedded

---
+++ vite.config.mjs
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.mjs',
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `@use "${path.join(process.cwd(), 'src/_mantine')}" as mantine;`,
+      },
+    },
+  },
+});
