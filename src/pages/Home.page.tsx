097a9285eab03b90c6b7281e6fa260b1dcd6b929
LA MORALES <labm42@yahoo.com>
2025-02-05 15:54:57 +0800

    0.1

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,11 @@
+import { HeaderMegaMenu } from '@/components/HeaderMegaMenu/HeaderMegaMenu';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
   return (
     <>
+      <HeaderMegaMenu/>
       <Welcome />
       <ColorSchemeToggle />
     </>
